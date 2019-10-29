22a2e91dc2f0186ba4c5748c0d0fc4afc121d53a
concigel <concigel@gmail.com>
2019-10-29 21:12:14 +0900

    add eslint

--- app/javascript/channels/todo_channel.js
+++ app/javascript/channels/todo_channel.js
@@ -9,7 +9,7 @@ consumer.subscriptions.create("TodoChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received(data) {
+  received(/* data */ ) {
     // Called when there's incoming data on the websocket for this channel
   }
 });
